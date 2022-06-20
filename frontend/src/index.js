e7615bc43234e91570496a5a704d87b1e603f365
Therese Nyman <tesanyman@gmail.com>
2022-06-20 12:42:27 +0200

    issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </>
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </>
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
