0ba63061a56bfda9afebde1abeaa845c488eebd1
Therese Nyman <tesanyman@gmail.com>
2022-06-21 23:31:37 +0200

    new test for netlify

--- frontend/src/components/Sidebars.js
+++ frontend/src/components/Sidebars.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import { setCookie } from "utils/cookieHelper";
 
 export default (props) => {
   return (
-    <Menu>
-      <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/about">
-        About
-      </a>
-      <a className="menu-item" href="/logout">
-        Sign out
-      </a>
-    </Menu>
+    <>
+      <Menu>
+        <a className="menu-item" href="/">
+          Home
+        </a>
+        <a className="menu-item" href="/about">
+          About
+        </a>
+        <a className="menu-item" href="/logout">
+          Sign out
+        </a>
+      </Menu>
+      <Link to="/about" className="login-btn">
+        <button className="firstpage-btn">about</button>
+      </Link>
+    </>
   );
 };
